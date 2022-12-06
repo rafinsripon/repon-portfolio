928c93ddc51f9ee1123413b12fffe35abb9bddee
rafinsripon <rafinsripon@gmail.com>
2022-12-06 15:02:34 +0600

    added projects folder

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import About from "../Pages/About/About";
 import Home from "../Pages/Home/Home";
+import Projects from "../Pages/Projects/Projects";
 import Resume from "../Pages/Resume/Resume";
 
 export const router = createBrowserRouter([
@@ -20,6 +21,10 @@ export const router = createBrowserRouter([
             {
                 path: '/resume',
                 element: <Resume />
+            },
+            {
+                path: '/projects',
+                element: <Projects />
             }
         ]
     }
