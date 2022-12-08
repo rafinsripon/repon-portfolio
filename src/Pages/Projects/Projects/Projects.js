a63c3a16f2f44790beb7ce788a0d00809be95f01
rafinsripon <rafinsripon@gmail.com>
2022-12-08 20:03:41 +0600

    project section css done

--- src/Pages/Projects/Projects/Projects.js
+++ src/Pages/Projects/Projects/Projects.js
@@ -1,19 +1,58 @@
 import React from "react";
 import "./Projects.css";
+import developer from "../../../Assets/Images/developer.png";
+import cyber from "../../../Assets/Images/cyber.png";
+import business from "../../../Assets/Images/business.png";
+import agency from "../../../Assets/Images/agency.png";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   return (
     <div className="project pt-10 pb-20">
       <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div className="text-center">
-          <p className="text-primary text-lg font-bold">Showcasing some of my best work</p>
+          <p className="text-primary text-lg font-bold">
+            Showcasing some of my best work
+          </p>
           <h2 className="font-[900] lg:text-6xl md:text-4xl text-4xl  text-white opacity-[0.8] mt-2 mb-4">
-          Recent Work.
+            Recent Work.
           </h2>
           <div class="animated-bar animatedbar_service"></div>
         </div>
-        <div className="grid lg:grid-cols-3 gap-6 mt-20">
-            
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 mt-20">
+          <div className="project_grid">
+            <div className="inner">
+              <img src={agency} alt="" />
+            </div>
+            <a href="https://rafinsripon.github.io/2.Agency-mega-one-HTML5-template/" target='_blank' rel="noreferrer">
+            <h2 className="text-2xl font-bold text-white">
+              Mega One - Creative Agency HTML5 Template
+            </h2>
+            <p className="text-primary mt-2">Web dedign / creative</p>
+            </a>
+          </div>
+          <div className="project_grid">
+            <div className="inner">
+              <img src={developer} alt="" />
+            </div>
+            <a href="https://rafinsripon.github.io/2.Agency-mega-one-HTML5-template/" target='_blank' rel="noreferrer">
+            <h2 className="text-2xl font-bold text-white">
+              Mega One - Creative Agency HTML5 Template
+            </h2>
+            <p className="text-primary mt-2">Web dedign / creative</p>
+            </a>
+          </div>
+          <div className="project_grid">
+            <div className="inner">
+              <img src={cyber} alt="" />
+            </div>
+            <a href="https://rafinsripon.github.io/2.Agency-mega-one-HTML5-template/" target='_blank' rel="noreferrer">
+            <h2 className="text-2xl font-bold text-white">
+              Mega One - Creative Agency HTML5 Template
+            </h2>
+            <p className="text-primary mt-2">Web dedign / creative</p>
+            </a>
+          </div>
         </div>
       </div>
     </div>
